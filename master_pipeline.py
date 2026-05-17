430836b278bfd0fd040683a55a18cc49e1a682b8
Miguel Lynch <miguellynch130304@gmail.com>
2026-05-17 10:15:29 -0500

    Cambios 1015

--- master_pipeline.py
+++ master_pipeline.py
@@ -140,6 +140,8 @@ class MasterPipeline:
                 cwd=str(PROJECT_ROOT),
                 capture_output=True,
                 text=True,
+                encoding='utf-8',
+                errors='replace',
                 timeout=600  # 10 minutos timeout
             )
             
